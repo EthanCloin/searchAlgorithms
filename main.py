dee553ec70c2e14baaae65399dad0f1476ca7366
Ethan Cloin <psnethan@gmail.com>
2022-04-03 20:08:36 -0400

    proceeds on user input and displays info msgs

--- main.py
+++ main.py
@@ -10,21 +10,24 @@ def main(stdscr):
     stdscr.clear()
 
     # define colors
-    curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_WHITE)
-    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_WHITE)
-    curses.init_pair(3, curses.COLOR_GREEN, curses.COLOR_WHITE)
-    curses.init_pair(4, curses.COLOR_YELLOW, curses.COLOR_WHITE)
-    curses.init_pair(5, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    curses.use_default_colors()
+    curses.init_pair(1, curses.COLOR_CYAN, -1)
+    curses.init_pair(2, curses.COLOR_RED, -1)
+    curses.init_pair(3, curses.COLOR_GREEN, -1)
+    curses.init_pair(4, curses.COLOR_YELLOW, -1)
+    curses.init_pair(5, curses.COLOR_WHITE, -1)
 
     # build description window
-    desc_win = curses.newwin(5, 30, 0, 0)
+    # desc_win = curses.newwin(5, 30, 0, 0)
     test_list = [x for x in range(11)]
     test_target = 4
-
+    #
     # search_test = LinearSearch(stdscr)
     # search_test.perform_search(test_list, test_target)
 
     bs_test = BinarySearch(stdscr)
+    # bs_test.update_status("TEST MSG", curses.color_pair(1))
+    # time.sleep(10)
     bs_test.perform_search(test_list, test_target)
 
 
--- searching/binary_search.py
+++ searching/binary_search.py
@@ -1,4 +1,5 @@
 import collections
+import sys
 
 from .search_strategy import SearchStrategy
 import curses
@@ -9,10 +10,26 @@ Bounds = collections.namedtuple('Bounds', ('lower', 'upper'))
 
 class BinarySearch(SearchStrategy):
 
+    H_PADDING = 4  # used for column spacing
+    V_PADDING = 4  # used for row spacing
+    OFFSET = 15  # used for relative pos from status_window - not used at all actually
+    # LONG_DELAY = 5
+    # SHORT_DELAY = 2
+    STATUS_WIN_CONFIG = (10, 50, 15, 0)
+
     def __init__(self, screen):
         super().__init__(screen)
         self.visited = []
         self.exec_log = []
+        self.status_window = curses.newwin(*self.STATUS_WIN_CONFIG)
+
+        self.EXIT_KEY = "q"
+        self.CONTINUE_KEY = "n"
+        self.CYAN = curses.color_pair(1)
+        self.RED = curses.color_pair(2)
+        self.GREEN = curses.color_pair(3)
+        self. YELLOW = curses.color_pair(4)
+        self.BLACK = curses.color_pair(5)
 
     def perform_search(self, nums, target):
         # init bounds
@@ -24,77 +41,91 @@ class BinarySearch(SearchStrategy):
         while True:
             # check if bounds get out of whack aka no solution
             if lower_bound > upper_bound or midpt > upper_bound or midpt < lower_bound:
+                self.update_status(f"Target does not exist!", self.RED)
                 return -1
 
+            self.update_status(f"Comparing midpoint:{nums[midpt]} to target:{target}", self.YELLOW)
             # check for solution
             if nums[midpt] == target:
                 self.draw_array(nums, nums[midpt], iteration, solution_found=True, solution_idx=midpt,
                                 bounds=Bounds(lower_bound, upper_bound))
+                self.update_status(f"Target found at index {midpt}!", self.GREEN)
                 time.sleep(10)
                 return midpt
 
             if nums[midpt] < target:
+
                 # confine search to the right half
                 self.draw_array(nums, nums[midpt], iteration, bounds=Bounds(lower_bound, upper_bound))
                 lower_bound = midpt + 1
                 midpt = (upper_bound + lower_bound) // 2
+                self.update_status(f"{nums[midpt]} is less than {target}...updating search range!", self.CYAN)
+                self.update_status(f"\nUpdated Range Indices: ({lower_bound}, {upper_bound})", self.CYAN)
             elif nums[midpt] > target:
                 # confine search to the left half
                 self.draw_array(nums, nums[midpt], iteration, bounds=Bounds(lower_bound, upper_bound))
                 upper_bound = midpt - 1
                 midpt = (upper_bound + lower_bound) // 2
+                self.update_status(f"{nums[midpt]} is greater than {target}...updating search range!", self.CYAN)
+                self.update_status(f"\nUpdated Range Indices: ({lower_bound}, {upper_bound})", self.CYAN)
             iteration += 1
 
     def draw_array(self, nums, cur_num, iteration, solution_found=False, solution_idx=-1, bounds: Bounds = None):
 
-        PADDING = 4  # used for both column spacing and window offset
-        DELAY = 2
-        CYAN = curses.color_pair(1)
-        RED = curses.color_pair(2)
-        GREEN = curses.color_pair(3)
-        YELLOW = curses.color_pair(4)
-        BLACK = curses.color_pair(5)
-        HEADER_MSG = "Performing a Binary Search!\n"
-
-        @staticmethod
-        def update_status(status_window, msg):
-            """display the supplied message in the status window"""
-            status_window.clear()
-            status_window.addstr(HEADER_MSG + msg)
-            status_window.refresh()
-
-        status_window = curses.newwin(PADDING, 30, 0, 0)
-        status_window.clear()
-        self.screen.refresh()
-        time.sleep(3)
-        status_window.refresh()
-
+        # initially draw all numbers yellow
         for idx, num in enumerate(nums):
             # paint all as yellow first
             str_num = str(num)
-            idx_padded = idx * PADDING
-            iter_padded = iteration + PADDING
-            self.screen.addstr(iter_padded, idx_padded, str_num, YELLOW)
+            idx_padded = idx * self.H_PADDING
+            iter_padded = iteration + self.V_PADDING
+            self.screen.addstr(iter_padded, idx_padded, str_num, self.YELLOW)
         self.screen.refresh()
 
+        # color bounds cyan, solution green, out of bounds black
         for idx, num in enumerate(nums):
             str_num = str(num)
-            idx_padded = idx * PADDING
-            iter_padded = iteration + PADDING
+            idx_padded = idx * self.H_PADDING
+            iter_padded = iteration + self.V_PADDING
 
             # paint solution green
             if idx == solution_idx:
-                self.screen.addstr(iter_padded, idx_padded, str_num, GREEN)
+                self.screen.addstr(iter_padded, idx_padded, str_num, self.GREEN)
                 continue
             # paint bounds cyan
             elif idx == bounds.lower or idx == bounds.upper:
-                self.screen.addstr(iter_padded, idx_padded, str_num, CYAN)
+                self.screen.addstr(iter_padded, idx_padded, str_num, self.CYAN)
                 continue
             # paint out of bounds black
             elif idx < bounds.lower:
-                self.screen.addstr(iter_padded, idx_padded, str_num, BLACK)
+                self.screen.addstr(iter_padded, idx_padded, str_num, self.BLACK)
             elif idx > bounds.upper:
-                self.screen.addstr(iter_padded, idx_padded, str_num, BLACK)
+                self.screen.addstr(iter_padded, idx_padded, str_num, self.BLACK)
 
             self.screen.refresh()
-        time.sleep(DELAY)
+
+    def update_status(self, msg, color):
+        """display the supplied message in the status window"""
+
+        HEADER_MSG = f"Performing a Binary Search!\n\tPress '{self.CONTINUE_KEY}' to proceed or '{self.EXIT_KEY}' to quit.\n"
+        HEADER_LINES = HEADER_MSG.count('\n')
+        self.screen.refresh()
+        self.status_window.clear()
+        self.status_window.addstr(0, 0, HEADER_MSG, self.CYAN)
+
+        self.status_window.addstr(HEADER_LINES + 1, 4, msg, color)
+        self.status_window.refresh()
+        self.exec_log.append(msg)
+        self.screen.refresh()
+
+        while True:
+            try:
+                user_input = self.screen.getkey()
+                # print(user_input)
+                if user_input is self.EXIT_KEY:
+                    sys.exit(0)
+                if user_input is self.CONTINUE_KEY:
+                    break
+
+            except (TypeError, Exception) as err:
+                print(f'caught {err}')
+
